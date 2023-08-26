5c1bc25943e78eb209b1388ef4d703ab2263af8f
baptiste66 <101480985+baptiste66@users.noreply.github.com>
2023-08-26 22:53:31 +0200

    projet terminer

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+// changement de myTopnav par main-navbar pour alignement 
 function editNav() {
   var x = document.querySelector(".main-navbar");
 
@@ -26,12 +27,152 @@ function launchModal() {
 const modaleClose = document.querySelector(".close");
 
 modaleClose.addEventListener("click", () => {
-  close ()
+  closeM ()
 })
 
-function close() {
+function closeM() {
   modalbg.style.display = "none";
 }
 
 //conditions du formulaires 
+const regexName = /^([A-Za-z|\s]{2,15})?([-]{0,1})?([A-Za-z|\s]{2,15})$/;
+const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const regexQuantity = /^([0-9]{1,2})$/;
 
+// dom element 
+const form = document.querySelector('form');
+const firstnameField = document.querySelector('#first');
+const lastnameField = document.querySelector('#last');
+const emailField = document.querySelector('#email');
+const birthdateField = document.querySelector('#birthdate');
+const quantityField = document.querySelector('#quantity');
+const conditionsCheckbox = document.querySelector('#checkbox1');
+const cityCheckBox = document.querySelectorAll("input[name='location']");
+
+// vérifie input
+firstnameField.addEventListener('input', () => checkInputValue(regexName, firstnameField, message.name)); 
+lastnameField.addEventListener('input', () => checkInputValue(regexName, lastnameField, message.name));
+emailField.addEventListener('input', () => checkInputValue(regexEmail, emailField, message.email));
+birthdateField.addEventListener('input', () => checkInputValue(birthdateField, message.birthdate));
+quantityField.addEventListener('input', () => checkInputValue(regexQuantity, quantityField, message.quantity));
+conditionsCheckbox.addEventListener('input', () => checkIfConditionsAccepted(conditionsCheckbox, message.conditions));
+cityCheckBox.forEach(radio => radio.addEventListener('change', () => checkIfCitySelected(cityCheckBox, message.city)));
+
+//dom element 
+const content = document.querySelector(".content");
+const modalSuccess = document.querySelector('.modal_success')
+
+// formulaire compare regex et input
+function validate(e) {
+    e.preventDefault();
+
+    
+    const isConditionsAccepted = checkIfConditionsAccepted(conditionsCheckbox, message.conditions);
+    const isCitySelected = checkIfCitySelected(cityCheckBox, message.city);
+    const isAgeSelected = checkAge(birthdateField, message.birthdate);
+    const isQuantityValid = checkInputValue(regexQuantity, quantityField, message.quantity);
+    const isEmailValid = checkInputValue(regexEmail, emailField, message.email);
+    const isLastNameValid = checkInputValue(regexName, lastnameField, message.name);
+    const isFirstNameValid = checkInputValue(regexName, firstnameField, message.name);
+
+    // si ok reset
+    if (isConditionsAccepted && isCitySelected && isAgeSelected && isQuantityValid && isEmailValid && isLastNameValid && isFirstNameValid) {
+        content.style.display = 'none';
+        modalSuccess.style.display = 'flex';
+        form.reset();
+    } 
+};
+
+
+// Check si les valeurs
+function checkInputValue(regex, element, message) {
+  const value = element.value;
+  if (!value || !regex.test(value)) {
+      setErrorMessage(element, message);
+      return false;
+  } 
+  hideErrorMessage(element);
+  return true; 
+};
+
+// check l'age 
+function checkAge (element, message) {
+  if (!element.value) {
+      setErrorMessage(element, message);
+      return false;
+  } 
+  hideErrorMessage(element);
+  return true;
+};
+
+// Check condition
+ function checkIfConditionsAccepted(element, message) {
+  if(!element.checked) {
+      setErrorMessage(element, message);
+      return false;
+  } 
+  hideErrorMessage(element);
+  return true;  
+};
+
+
+// Check ville selectionner
+function checkIfCitySelected(cities, message) {
+  const isChecked = Array.from(cities).some(radio => radio.checked);
+  if (!isChecked) {
+      setErrorMessage(cities[0], message);
+      return false;
+  };
+  hideErrorMessage(cities[0]);
+  return true;
+};
+
+//message erreur
+ const setErrorMessage = (element, message) => {
+  element.parentElement.setAttribute('data-error-visible', 'true');
+  element.parentElement.setAttribute('data-error', message);
+};
+//cache le message 
+const hideErrorMessage = element => {
+  element.parentElement.removeAttribute('data-error-visible');
+  element.parentElement.removeAttribute('data-error');
+};
+
+
+const message = {
+  name: 'Veuillez entrer 2 caractères ou plus pour le champ. ',
+  email: 'Vous devez entrer une adresse mail valide.',
+  birthdate: 'Vous devez entrer votre date de naissance.',
+  quantity: 'Vous devez choisir une quantité.',
+  city: 'Vous devez choisir une option.',
+  conditions: `Vous devez vérifier que vous acceptez les termes et conditions.`,
+};
+
+// Open / Close Modal Form
+
+const btnSignup = document.querySelectorAll(".btn_signup");
+const btnSubmit = document.querySelectorAll(".btn-submit");
+
+btnSignup.forEach(btn => { btn.addEventListener('click', () => form.style.display = "flex") });
+
+
+// Send Form
+form.addEventListener('submit', function(event) {
+  validate(event);
+});
+
+    document.querySelector('.btn').addEventListener('click', () => {
+        modalbg.style.display = "none";
+        modalSuccess.style.display = "none"; 
+        resetForm();
+    });
+
+    function resetForm() {
+      // Réinitialiser les champs du formulaire
+      form.reset();
+    
+      // Rétablir l'affichage par défaut
+      content.style.display = 'block';
+      modalSuccess.style.display = 'none';
+      modalbg.style.display = 'none';
+    }
